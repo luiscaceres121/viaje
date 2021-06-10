85699585ac413fdacb160c7cc9472e6d88fd7431
luiscaceres121 <85408539+luiscaceres121@users.noreply.github.com>
2021-06-09 21:58:45 -0400

    Add files via upload
    ricomida subida1

---
+++ Ricomida - copia/Assets/js/script.js
@@ -0,0 +1,4 @@
+document.querySelector("body > main > section.section_ingre_prepa")
+document.querySelector("head > link:nth-child(4)")-webkit-text-size-adjust: 100%;
+-webkit-tap-highlight-color: transparent;
+-
